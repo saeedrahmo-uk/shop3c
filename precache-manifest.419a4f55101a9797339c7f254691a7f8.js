24d1d41042a20aab337b6fa9db1fed39226e61d2
saeed rahmani <saeedrahmani@yandex.com>
2020-05-05 20:03:26 +0430

    Updates

---
+++ precache-manifest.419a4f55101a9797339c7f254691a7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35843d697e801cfea9a609ff3a173415",
+    "url": "/shop3c/index.html"
+  },
+  {
+    "revision": "60ed36439ba1ead36f8f",
+    "url": "/shop3c/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f88235ae388f8c6ca8c",
+    "url": "/shop3c/static/js/2.4df14d90.chunk.js"
+  },
+  {
+    "revision": "60ed36439ba1ead36f8f",
+    "url": "/shop3c/static/js/main.5e7ed9e4.chunk.js"
+  },
+  {
+    "revision": "c0aed578ef9ece8d38c0",
+    "url": "/shop3c/static/js/runtime-main.e8259d82.js"
+  }
+]);
